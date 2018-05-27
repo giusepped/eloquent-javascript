272c9e0cc0f6fb203c369a178f6927fdecfb37ea
giuseppe de santis <giusdesan@gmail.com>
2018-05-27 12:44:36 +0100

    add constant route

--- chapter07/robot.js
+++ chapter07/robot.js
@@ -92,11 +92,24 @@ VillageState.random = function(parcelCount = 5) {
         do {
             place = randomPick(Object.keys(roadGraph));
         } while (place == address);
-        parcels.push({place, address});
+        parcels.push({ place, address });
     }
     return new VillageState("Post Office", parcels);
 };
 
-runRobot(VillageState.random(), randomRobot);
+// runRobot(VillageState.random(), randomRobot);
 
+const mailRoute = [
+    "Alice's House", "Cabin", "Alice's House", "Bob's House",
+    "Town Hall", "Daria's House", "Ernie's House",
+    "Grete's House", "Shop", "Grete's House", "Farm",
+    "Marketplace", "Post Office"
+];
+
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+    return { direction: memory[0], memory: memory.slice(1) };
+}
 
