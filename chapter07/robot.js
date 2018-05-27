e1cc08bcd5c246d3b7ce2a27ca0753a3bd5e5adb
giuseppe de santis <giusdesan@gmail.com>
2018-05-27 13:17:54 +0100

    add shortest route

--- chapter07/robot.js
+++ chapter07/robot.js
@@ -113,3 +113,28 @@ function routeRobot(state, memory) {
     return { direction: memory[0], memory: memory.slice(1) };
 }
 
+// find the shortest route
+function findRoute(graph, from, to) {
+    let work = [{ at: from, route: [] }];
+    for (var i = 0; i < work.length; i++) {
+        let { at, route } = work[i];
+        for (let place of graph[at]) {
+            if (place == to) return route.concat(place);
+            if (!work.some(s => s.at == place)) {
+                work.push({ at: place, route: route.concat(place) })
+            }
+        }
+    }
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+    return { direction: route[0], memory: route.slice(1) };
+}
